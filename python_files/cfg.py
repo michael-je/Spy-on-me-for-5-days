964805f72c1517cfd9ebf59f8c77b30be1f691c4
michael <mikkmakk89@gmail.com>
2021-03-25 16:43:44 +0100

    purge __pycache__, update .gitignore and update help info

--- python_files/cfg.py
+++ python_files/cfg.py
@@ -4,9 +4,9 @@ import secrets
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
 # credentials for twitch bot account are stored in a separate file
-NICK = secrets.NICK
-CHAN = secrets.CHAN
-PASS = secrets.PASS
+NICK = secrets.NICK # bot channel name
+PASS = secrets.PASS # bot oauth token https://twitchapps.com/tmi/
+CHAN = secrets.CHAN # streaming channel name
 
 threads_delay = 1 # delay time inserted into thread loops in seconds
 twitch_chat_spam_filter_seconds = 2 # how long the spam filter should ignore messages after last received
