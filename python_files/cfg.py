4482120851e9a5aa0d4c2ec555b55ae743aeff6b
Your Name <michaeljoseph195@gmail.com>
2021-03-25 22:06:51 +0100

    improve command match protocol

--- python_files/cfg.py
+++ python_files/cfg.py
@@ -8,16 +8,40 @@ NICK = secrets.NICK # bot channel name
 PASS = secrets.PASS # bot oauth token https://twitchapps.com/tmi/
 CHAN = secrets.CHAN # streaming channel name
 
+
 threads_delay = 1 # delay time inserted into thread loops in seconds
 twitch_chat_spam_filter_seconds = 2 # how long the spam filter should ignore messages after last received
 
+
 # refer to the example for correct protocol
 # use the keyword None if no parameters are needed at an index
-command_matches = {
-	"example":[
-		["word", "or", "words", "to"], # match against any word in this list
+command_matches = [
+	[
+		"command name", # command name, duh
+		"obs", # which program interface to target 
+		["match", "any", "of", "these"], # match against any word in this list
 		["match this phrase"], # match against any exact phrase in this list
 		[0, 127], # numeric range to match
-		"obs", # which program interface to target
+	],
+	[
+		"desktop",
+		"obs",
+		["desktop"], 
+		None,
+		None,
+	],
+	[
+		"facecam",
+		"obs",
+		["front", "face", "facecam"],
+		None,
+		None,
+	],
+	[
+		"roomcam",
+		"obs",
+		["room"],
+		None,
+		None,
 	]
-}
+]
