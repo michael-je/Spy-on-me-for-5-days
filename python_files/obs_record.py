fa6b3bc690e6b84b0dd6a49d62998021190a7049
Michael Joseph <michaeljoseph195@gmail.com>
2021-04-13 18:52:18 +0200

    create script to automatically split recordings in obs

---
+++ python_files/obs_record.py
@@ -0,0 +1,60 @@
+import cfg
+import secrets
+import utilities
+
+from time import sleep
+import asyncio
+from obswsrc import OBSWS
+from obswsrc.requests import ResponseStatus, StartRecordingRequest, StopRecordingRequest
+
+loop = asyncio.get_event_loop()
+
+
+async def start_stop_recording():
+
+	try:
+		# Start recording initially
+		async with OBSWS('localhost', 4444, secrets.obs_wss_pass) as obsws:
+			response = await obsws.require(StartRecordingRequest())
+			if response.status == ResponseStatus.OK:
+				print("OBS Has started recording")
+			else:
+				print("OBS recording error:", response.error)
+
+		terminate = False
+		while not terminate:
+			async with OBSWS('localhost', 4444, secrets.obs_wss_pass) as obsws:
+							
+				sleep(cfg.obs_recording_time_split)
+				terminate = utilities.get_state("terminate_flag")
+
+				# Stop recording
+				response = await obsws.require(StopRecordingRequest())
+				if not response.status == ResponseStatus.OK:
+					print("OBS recording error:", response.error)
+
+				sleep(0.3)
+
+				# Start recording
+				response = await obsws.require(StartRecordingRequest())
+				if not response.status == ResponseStatus.OK:
+					print("OBS recording error:", response.error)
+
+				print("OBS record split")
+
+	except KeyboardInterrupt:
+		async with OBSWS('localhost', 4444, secrets.obs_wss_pass) as obsws:
+			response = await obsws.require(StopRecordingRequest())
+			if not response.status == ResponseStatus.OK:
+				print("OBS recording error:", response.error)
+		return
+			
+
+
+def main():
+	loop.run_until_complete(start_stop_recording())
+	print("OBS has finished recording")
+
+
+if __name__ == "__main__":
+	main()
