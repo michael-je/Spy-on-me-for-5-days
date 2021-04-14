ef4df6ea4a4aa702e08adc8a07e28c62c0c6b477
Michael Joseph <michaeljoseph195@gmail.com>
2021-04-14 18:09:41 +0200

    improve environment_setup.sh

--- other/environment_setup.sh
+++ other/environment_setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# launch main pd_sketch, sleep to give it time to connect to jack
-#pd $(cd ../ && pwd)/pd_files/main_palette/palette1.pd &
-#sleep 1
+# change terminate_flag to 0
+sed -i "s|terminate_flag 1|terminate_flag 0|" $(dirname $(realpath $0))/states.txt
 
 # launch catia
 catia &
+
+# launch obs
+#obs &
+
+# launch main pd_sketch, sleep to give it time to connect to jack
+pd $(dirname $(dirname $(realpath $0)))/pd_files/main_palette/palette1.pd &
+
+# alias8 python script
+python3 $(dirname $(dirname $(realpath $0)))/python_files/alias8_osc.py &
+
+sleep 1
 
 # run jack_setup.sh
 $(dirname $(realpath $0))/jack_setup.sh
 
 # twitch bot python script
-python3 $(dirname $(realpath $0))/../python_files/twitch_bot.py
-
-# alias8 python script
-python3 $(dirname $(realpath $0))/../python_files/alias8_osc.py &
-
-# launch obs
+python3 $(dirname $(dirname $(realpath $0)))/python_files/twitch_bot.py
